0ddd9160c91dc3de2fa5ba931d0e26961eccf3b6
Rasul230405 <resulinci999@gmail.com>
2021-05-17 06:11:14 +0000

    adding entry pages - topic.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,5 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('', views.index, name="index"),
+    path('topics/', views.topics, name="topics"),
+    path('topics/<int:topic_id>/', views.topic, name="topic"),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
     """Home Page Of Learning Log"""
     return render(request,"learning_logs/index.html")
+
+def topics(request):
+    topics = Topic.objects.order_by("date_added")
+    context = {'topics':topics}
+    return render(request,"learning_logs/topics.html",context)
+
+def topic(request, topic_id):
+    """Entry page""" 
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request,"learning_logs/topic.html",context)   
