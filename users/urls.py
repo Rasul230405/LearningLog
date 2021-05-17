134d9ec84eb6af97276b07fef1cf81d3ad194bf3
Rasul230405 <resulinci999@gmail.com>
2021-05-17 14:15:49 +0000

    adding register page

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout/', views.logout_view, name="logout"),
+    path('register/', views.register, name="register"),
 ]
