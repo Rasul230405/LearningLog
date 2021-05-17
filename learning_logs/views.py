f9c57bddc68f0ad3b51f2fc3f891b9f2c6bbb265
Rasul230405 <resulinci999@gmail.com>
2021-05-17 08:43:01 +0000

    Adding new entry.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """Home Page Of Learning Log"""
@@ -17,3 +20,32 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request,"learning_logs/topic.html",context)   
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+
+    context = {'form': form}    
+    return render(request,"learning_logs/new_topic.html",context)
+
+def new_entry(request, topic_id):
+    """User add entry assosited with topic"""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            my_entry = form.save(commit=False)
+            my_entry.topic = topic
+            my_entry.save()
+            return HttpResponseRedirect(reverse('topic',args=[topic_id]))
+
+    context = {'topic': topic,'form': form}
+    return render(request,"learning_logs/new_entry.html",context)        
