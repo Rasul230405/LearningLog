23e5fd0e1fbddcdc2a6e86b595449f24d88fecdf
Rasul230405 <resulinci999@gmail.com>
2021-05-18 06:16:21 +0000

    add owner

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """Create models"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self):
         """Return string represent of the model"""
