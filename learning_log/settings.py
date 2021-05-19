231541905993eab3f9425b72bf16e06876ad05a9
reuneum <resulinci999@gmail.com>
2021-05-19 07:38:10 +0000

    add allowed host

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#wgjgb_g)kv8s6po&)v*bfxm(k3&a5tx2dy567a0ox$e&)a9xs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['learning-log-app-my-app.herokuapp.com']
 
 
 # Application definition
