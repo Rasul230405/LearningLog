a4f7d0d1d3ca461a9fc691210e77c55d5fa839fc
Rasul230405 <resulinci999@gmail.com>
2021-05-24 10:03:11 +0000

    fix email verification errors

--- users/views.py
+++ users/views.py
@@ -45,14 +45,14 @@ def register(request):
 def activate_account(request, uidb64, token):
     try:
         uid = force_bytes(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=token)
+        user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()    
         login(request, user)
-        return HttpResponse('Your account has been activate successfully')
+        return HttpResponseRedirect(reverse('users:success'))
     else:
         return HttpResponse('Token is invalid!')    
 
